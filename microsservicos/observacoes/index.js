70daec000f6a1186a71a1a3e6865b18bb10607ae
Bruno Ferreira da Silva <bruno_silva2711@yahoo.com.br>
2025-10-06 21:58:17 -0300

    construção do microsserviço barramento

--- microsservicos/observacoes/index.js
+++ microsservicos/observacoes/index.js
@@ -1,23 +1,34 @@
 const express = require('express')
 const app = express()
+const {v4: uuidv4} = require('uuid')
 app.use(express.json())
 /*
 {
     1: [
-        {id: 1000, texto: comprar açúcar,}
+        {id: 1000, texto: comprar açúcar, idLembrete: 1}
+        {id: 1001, texto: 2 colheres de pó, idLembrete: 1}
     ]
 }
 */
 const baseObservacoes = {}
 //POST /lembretes/1/observacoes
 app.post('/lembretes/:id/observacoes', (req, res) => {
-
+    const idObs = uuidv4()
+    const { texto } = req.body
+    const observacao = {
+        id: idObs,
+        texto
+    }
+    const observacoes = baseObservacoes[req.params.id] || []
+    observacoes.push(observacao)
+    baseObservacoes[req.params.id] = observacoes
+    res.status(201).json(observacao)
 })
 
 //GET
 app.get('/lembretes/:id/observacoes', (req, res) => {
-
+    res.json(baseObservacoes[req.params.id])
 })
 
 const port = 5000
-app.listen(port, console.log(`Observações. Porta ${porta}.`))
+app.listen(port, console.log(`Observações. Porta ${port}.`))
